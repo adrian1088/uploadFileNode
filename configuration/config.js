5fc63243322da2ef205ae08e244a134db46cd2bb
adrian1088 <adrianenelpozo@gmail.com>
2016-07-28 09:31:27 -0400

    Add files via upload

---
+++ configuration/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'folder_upload': './public/uploads', // frase para generar los tokens
+    'database': 'mongodb://adrian1088:Jmal2na2@ds021943.mlab.com:21943/chatoon',
+    'port': 3200
+};
