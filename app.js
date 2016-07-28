4345d82eae320093f00e38cef249f79ca3861d3d
Jose Maldonado Pozo <adrianenelpozo@gmail.com>
2016-07-28 10:00:38 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app     = express();
 var multer  = require('multer');
 var config  = require('./configuration/config'); // archivo de configuración
+var done    =     false ; 
 
 // Multer Storage para mantener la extenteción del archivo.
 var storage = multer.diskStorage({
@@ -19,6 +20,11 @@ var port = process.env.PORT || config.port;
 var upload = multer({ storage: storage });
 
 app.use(express.static(__dirname + '/public'));
+
+//vista de form para subir archivos
+app.get('/', function (req, res, next) {
+	res.sendfile ("views/index.html");
+});
 
 // Ruta para subir el archivo.
 app.post('/subir', upload.array('file',2), function (req, res, next) {
--- configuration/config.js
+++ configuration/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     'folder_upload': './public/uploads', // frase para generar los tokens
     'database': 'mongodb://adrian1088:Jmal2na2@ds021943.mlab.com:21943/chatoon',
-    'port': 3200
+    'port': 8080
 };
